174e89a4ee42e3491501d98e5b2c330a71342d8b
maxkram <max.kramer@live.com>
2022-06-03 20:08:53 -0400

    refactor: modify cjsToEsm.js to ECMAScript notation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,6 @@
 export const parseArgs = () => {
-    // Write your code here 
+  for (let i = 2; i < process.argv.length; i += 2) {
+    process.stdout.write(`${process.argv[i]} is ${process.argv[i + 1]}`);
+    if (i + 2 < process.argv.length) process.stdout.write(', ');
+  }
 };
---
+++ src/modules/cjsToEsm.mjs
@@ -0,0 +1,32 @@
+import { sep } from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c';
+import a from './files/a.json';
+import b from './files/b.json';
+
+const random = Math.random();
+
+let unknownObject;
+
+if (random > 0.5) {
+  unknownObject = a;
+} else {
+  unknownObject = b;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const createMyServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+export default {
+  unknownObject,
+  createMyServer,
+};
